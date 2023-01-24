a77c74ef1c600b581011bac625d95929e0fb146d
Enzo Rudy SEKKAI <enzorudy.sekkai@outlook.com>
2023-01-24 19:06:33 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import { useState } from 'react'
 
-const History = ({allClicks}) => {
-    if (allClicks.length === 0) {
-        return (
-            <div>
-                the app is used by pressing the buttons
-            </div>
-        )
-    }
-    return (
-        <div>
-            button press history: {allClicks.join(' ')}
-        </div>
-    )
-}
-
-const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-        {text}
-    </button>
-)
 const App = () => {
-    const [clicks, setClicks] = useState({
-        left: 0, right: 0, allClicks: []
-    })
-
-    const handleLeftClick = (side) => () =>
-        setClicks({ ...clicks, left: clicks.left + 1 , allClicks: clicks.allClicks.concat(side) })
-
-    const handleRightClick = (side) => {
-        setClicks({ ...clicks, right: clicks.right + 1 , allClicks: clicks.allClicks.concat(side) })
-    }
-
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
     return (
         <div>
-            {clicks.left}
-            <Button handleClick={handleLeftClick("L")} text="left" />
-            <Button handleClick={() => handleRightClick("R")} text="right" />
-            {clicks.right}
-            <History allClicks={clicks.allClicks} />
+            code here
         </div>
     )
 }
