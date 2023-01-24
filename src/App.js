6048aa73a56b944a3c3c00908cec98dce23982fa
Enzo Rudy SEKKAI <enzorudy.sekkai@outlook.com>
2023-01-24 19:46:37 +0100

    1.9

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ const Button = ({ handleClick, text }) => {
 }
 
 const Statistics = ({ good, neutral, bad , all, average, positive}) => {
+
+    if (all === 0) {
+        return (
+            <div>
+                <h1>statistics</h1>
+                <p>No feedback given</p>
+            </div>
+        )
+    }
     return (
         <div>
             <h2>statistics</h2>
