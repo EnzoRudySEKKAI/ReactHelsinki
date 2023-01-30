0fb22922c906f052df384d9fcf60a8454213ede3
Rudy SEKKAI <enzorudy.sekkai@yahoo.com>
2023-01-30 16:19:11 +0100

    2.10

--- src/App.js
+++ src/App.js
@@ -1,70 +1,25 @@
 import { useState } from 'react'
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
     const [persons, setPersons] = useState([
         { name: 'Arto Hellas', number: '39-44-5323523', id: 1 }
     ])
-    const [newName, setNewName] = useState('')
-    const [newNumber, setNewNumber] = useState('')
+
     const [filter, setFilter] = useState('')
 
-
     const personsToShow = filter === '' ? persons : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-
-    const addPerson = (event) => {
-        event.preventDefault()
-        if(persons.find(person => person.name === newName)) {
-            alert(`${newName} is already added to phonebook`)
-        }
-        else{
-            const personObject = {
-                name: newName,
-                number: newNumber,
-                id: persons.length + 1
-            }
-            setPersons(persons.concat(personObject))
-            setNewName('')
-            setNewNumber('')
-        }
-
-    }
-
-    const handleFilterChange = (event) => {
-        setFilter(event.target.value)
-    }
-
-    const handleNameChange = (event) => {
-        setNewName(event.target.value)
-    }
-
-    const handleNumberChange = (event) => {
-        setNewNumber(event.target.value)
-    }
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
-                <div>
-                    filter shown with <input onChange={handleFilterChange}/>
-                </div>
-            </form>
-            <h2>Add a new</h2>
-            <form onSubmit={addPerson}>
-                <div>
-                    name: <input value={newName} onChange={handleNameChange}/>
-                </div>
-                <div>
-                    number: <input value={newNumber} onChange={handleNumberChange}/>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
+            <Filter setFilter={setFilter}/>
+            <h3>Add a new</h3>
+            <PersonForm persons={persons} setPersons={setPersons}/>
             <h2>Numbers</h2>
-            <ul>
-                {personsToShow.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
-            </ul>
+            <Persons persons={personsToShow}/>
         </div>
     )
 }
---
+++ src/components/Filter.js
@@ -0,0 +1,17 @@
+
+const Filter = ({setFilter}) => {
+
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+    }
+
+    return (
+        <form onChange={handleFilterChange}>
+            <div>
+                filter shown with <input onChange={handleFilterChange}/>
+            </div>
+        </form>
+    );
+}
+
+export default Filter;
