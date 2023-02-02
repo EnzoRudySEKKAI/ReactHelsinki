40f200028dc4f4721a44d211302ac24a75a27b68
Rudy SEKKAI <enzorudy.sekkai@yahoo.com>
2023-02-02 20:58:11 +0100

    course css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from "./components/Notification";
 
 
 const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState('some error happened...')
 
     const getAllNotes = () => {
         noteService.getAll().then(response => {
@@ -39,17 +41,25 @@ const App = () => {
         const note = notes.find(n => n.id === id)
         const changedNote = { ...note, important: !note.important }
 
-        noteService.update(id, changedNote).then(response => {
-            setNotes(notes.map(note => note.id !== id ? note : response))
-        }).catch(error => {
-            alert(`the note '${note.content}' was already deleted from server`)
-            setNotes(notes.filter(n => n.id !== id))
+        noteService
+            .update(id, changedNote).then(returnedNote => {
+            setNotes(notes.map(note => note.id !== id ? note : returnedNote))
         })
+            .catch(error => {
+                setErrorMessage(
+                    `Note '${note.content}' was already removed from server`
+                )
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000)
+                setNotes(notes.filter(n => n.id !== id))
+            })
     }
 
     return (
         <div>
             <h1>Notes</h1>
+            <Notification message={errorMessage} />
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all'}
